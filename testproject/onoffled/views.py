932f214ef9af75bc0e0efdc09413511b96d4ab10
Tanakornguy <guytanakornkhuntamanee@gmail.com>
2023-02-14 23:20:08 +0700

    .

--- testproject/onoffled/views.py
+++ testproject/onoffled/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render, redirect
-import Jetson.GPIO as GPIO
+#import Jetson.GPIO as GPIO
 
 # set the GPIO pin for the LED
 LED_GPIO = 18
 
 # initialize the GPIOs
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_GPIO, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(LED_GPIO, GPIO.OUT)
 
 # Create your views here.
 def led_on(request):
-    GPIO.output(LED_GPIO, GPIO.HIGH)
+    #GPIO.output(LED_GPIO, GPIO.HIGH)
     return redirect('led')
 
 def led_off(request):
-    GPIO.output(LED_GPIO, GPIO.LOW)
+    #GPIO.output(LED_GPIO, GPIO.LOW)
     return redirect('led')
 
 def led(request):
     return render(request, 'led.html')
 
 # clean up the GPIOs when the server shuts down
-def cleanup():
-    GPIO.cleanup()
+#def cleanup():
+    #GPIO.cleanup()
 
-import atexit
-atexit.register(cleanup)
+#import atexit
+#atexit.register(cleanup)
